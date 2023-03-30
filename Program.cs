ed50bf57337eeac8fad69f2e41c15ae9e0dbbbb8
Chandra Mohan.S <66741917+Chandra-mohan31@users.noreply.github.com>
2023-03-30 09:09:08 +0530

    personality trait finder

--- Program.cs
+++ Program.cs
@@ -1,60 +1,201 @@
-﻿using System.Diagnostics;
-
-namespace SellStock
+﻿namespace Task_24_03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Stocks Problem");
-            //int[] stocks = { 7 ,1, 5, 3, 6, 4 };
-            int[] stocks =  { 1,2,4,2,5,7,2,4,9,0,9};
+            Console.WriteLine("Personality trait application!");
+            //personality trait application
 
-            Console.WriteLine("The maximum profit you can make is : " + CalcProfitOptimised(stocks));
-            //Console.Write(CalcProfitOptimised(stocks));
+            //general information
+            Console.WriteLine("Enter your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the date you are born: ");
+
+            string date = Console.ReadLine();
+            Console.WriteLine("Enter your month you were born : ");
+
+            string month = Console.ReadLine();
+            Console.WriteLine("Enter the year you were born: ");
+
+            string year = Console.ReadLine();   
+
+            string Dob = date + "/" + month +"/" + year;
+
+            //Be a mentalist
+
+            Console.WriteLine("Are you an Introvert: yes//no\n");
+            string isIntrovert = Console.ReadLine();
+            Console.WriteLine("Are you a happy inside: yes//no\n");
+            string isHappyInside = Console.ReadLine();
+            Console.WriteLine("Rate yourself on the basis of peacefulness: 1-10\n");
+            int peacefullnes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Rate yourself on the basis of using social media: 1-10\n");
+            int useOfSocialMedia = Convert.ToInt32(Console.ReadLine());
 
 
-        }
-        static int CalcProfit(int[] stocks)
-        {
-            int max_profit = 0;
-            
-            for (int i = 0; i < stocks.Length - 1; i++)
+            if (isIntrovert == "yes" && isHappyInside == "yes")
             {
-                for (int j = i + 1; j < stocks.Length; j++)
+                Console.WriteLine("You are a happy person with genuine feelings!");
+            }
+            else if (isIntrovert == "yes" && isHappyInside == "no")
+            {
+                Console.WriteLine("You are Broken,dont worry things will get right!");
+            }
+            else if(isHappyInside == "yes" && peacefullnes > 5)
+            {
+                Console.WriteLine("You are a peacefull and will always be happy!");
+            }else if(useOfSocialMedia >= 6)
+            {
+                Console.WriteLine("Do you honestly get value of it: yes//no\n");
+                string resp = Console.ReadLine();
+                if(resp == "yes" && peacefullnes > 5)
                 {
-                    if (stocks[j] > stocks[i])
-                    {
-                        max_profit = Math.Max((stocks[j] - stocks[i]), max_profit);
-                    }
+                    Console.WriteLine("You are modern and with good self control!");
                 }
             }
-            return max_profit;
+
+            //Mimic Ptolemy
+
+            //Console.WriteLine("Lets find your Partner:\n");
+
+            Console.WriteLine( $"Your zodiac sign : {zodiac_sign(12, month)}");
+
+
+
+            //calendar man
+            Console.WriteLine(CalculateAge(Convert.ToDateTime(Dob)));
+            
         }
-        //int[] stocks = { 7, 1, 5, 3, 6, 4 };
-
-        static int CalcProfitOptimised(int[] stocks)
+        static string CalculateAge(DateTime Dob)
         {
-            int max_profit = 0;
-            int curr_buy_day = 0;
-            int curr_sell_day = 1;
-            
-            while(curr_sell_day < stocks.Length && curr_buy_day < stocks.Length) {
-                if (stocks[curr_sell_day] - stocks[curr_buy_day] > 0)
+            DateTime Now = DateTime.Now;
+            int Years = new DateTime(DateTime.Now.Subtract(Dob).Ticks).Year - 1;
+            //Console.WriteLine("Years.." + Years);
+            DateTime PastYearDate = Dob.AddYears(Years);
+           
+
+            int Months = 0;
+            for (int i = 1; i <= 12; i++)
+            {
+                if (PastYearDate.AddMonths(i) == Now)
                 {
-                    max_profit = Math.Max((stocks[curr_sell_day] - stocks[curr_buy_day]), max_profit);
-                    curr_sell_day++;
+                    Months = i;
+                    break;
                 }
-                else
+                else if (PastYearDate.AddMonths(i) >= Now)
                 {
-                    curr_buy_day=curr_sell_day;
-                    curr_sell_day++;
+                    Months = i - 1;
+                    break;
                 }
             }
+            
+            
+            return $"Your current Age is : {Convert.ToString(Years)} years {Convert.ToString(Months)} months";
+        }
 
-            return max_profit;
+        static string zodiac_sign(int day, string month)
+        {
+            string astro_sign = "";
 
             
+            if (month == "12")
+            {
+
+                if (day < 22)
+                    astro_sign = "Sagittarius";
+                else
+                    astro_sign = "capricorn";
+            }
+
+            else if (month == "1")
+            {
+                if (day < 20)
+                    astro_sign = "Capricorn";
+                else
+                    astro_sign = "aquarius";
+            }
+
+            else if (month == "2")
+            {
+                if (day < 19)
+                    astro_sign = "Aquarius";
+                else
+                    astro_sign = "pisces";
+            }
+
+            else if (month == "3")
+            {
+                if (day < 21)
+                    astro_sign = "Pisces";
+                else
+                    astro_sign = "aries";
+            }
+            else if (month == "4")
+            {
+                if (day < 20)
+                    astro_sign = "Aries";
+                else
+                    astro_sign = "taurus";
+            }
+
+            else if (month == "5")
+            {
+                if (day < 21)
+                    astro_sign = "Taurus";
+                else
+                    astro_sign = "gemini";
+            }
+
+            else if (month == "6")
+            {
+                if (day < 21)
+                    astro_sign = "Gemini";
+                else
+                    astro_sign = "cancer";
+            }
+
+            else if (month == "7")
+            {
+                if (day < 23)
+                    astro_sign = "Cancer";
+                else
+                    astro_sign = "leo";
+            }
+
+            else if (month == "8")
+            {
+                if (day < 23)
+                    astro_sign = "Leo";
+                else
+                    astro_sign = "virgo";
+            }
+
+            else if (month == "9")
+            {
+                if (day < 23)
+                    astro_sign = "Virgo";
+                else
+                    astro_sign = "libra";
+            }
+
+            else if (month == "10")
+            {
+                if (day < 23)
+                    astro_sign = "Libra";
+                else
+                    astro_sign = "scorpio";
+            }
+
+            else if (month == "11")
+            {
+                if (day < 22)
+                    astro_sign = "scorpio";
+                else
+                    astro_sign = "sagittarius";
+            }
+
+            return astro_sign;
         }
     }
 }
